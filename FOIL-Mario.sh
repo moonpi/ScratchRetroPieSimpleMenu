6f47848fbf7ccec66d4f73111753a7b7e38ad6db
Scottt <scott.geyer@eagles.usm.edu>
2017-02-23 14:54:29 -0600

    Update FOIL-Mario.sh

--- FOIL-Mario.sh
+++ FOIL-Mario.sh
@@ -1,13 +1,8 @@
-#!/bin/bash
-#Disable DPMS
-xset -dpms
-# Clean up previously running apps, gracefully at first then harshly
-killall -TERM chromium 2>/dev/null;
-killall -TERM matchbox-window-manager 2>/dev/null;
-sleep 2;
-killall -9 chromium 2>/dev/null;
-killall -9 matchbox-window-manager 2>/dev/null;
-# Launch window manager without title bar.
-exec matchbox-window-manager &
-# Launch browser.
-chromium -app="https://scratch.mit.edu/projects/64057444/#fullscreen"
+
+#!/bin/sh
+xset -dpms # disable DPMS (Energy Star) features.
+xset s off # disable screen saver
+xset s noblank # don't blank the video device
+matchbox-window-manager &
+midori -e Fullscreen -a https://phosphorus.github.io/app.html?id=21554369&turbo=false&full-screen=true
+# This uses Phosophorus and Midori to run FOIL Mario by Punkbass (remixed from FlunkyLizardBun and PaulKoning)
