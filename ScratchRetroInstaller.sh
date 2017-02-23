c55fb24ef35a012d11927205ab9e2b7826a83888
Scottt <scott.geyer@eagles.usm.edu>
2017-02-23 12:55:53 +0000

    Update ScratchRetroInstaller.sh

--- ScratchRetroInstaller.sh
+++ ScratchRetroInstaller.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get update
 #sudo apt-get dist-upgrade
-sudo apt-get -y install matchbox chromium x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3
+sudo apt-get -y install matchbox chromium-browser x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3
 
 #create folders for install
 
