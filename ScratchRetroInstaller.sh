7a898f0df7160acf3264997576ddfe84856553ff
Scottt <scott.geyer@eagles.usm.edu>
2017-02-23 12:59:05 +0000

    Update ScratchRetroInstaller.sh

--- ScratchRetroInstaller.sh
+++ ScratchRetroInstaller.sh
@@ -6,6 +6,9 @@ sudo apt-get update
 #sudo apt-get dist-upgrade
 sudo apt-get -y install matchbox chromium-browser x11-xserver-utils ttf-mscorefonts-installer xwit sqlite3 libnss3
 
+echo -e "\n Switch your x11 settings to anybody \n" 
+sudo dpkg-reconfigure x11-common
+
 #create folders for install
 
 sudo mkdir /home/pi/RetroPie/roms/scratch
