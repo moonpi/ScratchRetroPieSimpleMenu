b5c71c0e1196d98107861c510ae8181e705134be
Scottt <scott.geyer@eagles.usm.edu>
2017-02-23 15:06:54 -0600

    Delete O.sh

--- O.sh
+++
@@ -1,8 +0,0 @@
-
-#!/bin/sh
-xset -dpms # disable DPMS (Energy Star) features.
-xset s off # disable screen saver
-xset s noblank # don't blank the video device
-matchbox-window-manager &
-midori -e Fullscreen -a https://phosphorus.github.io/app.html?id=15945630&turbo=false&full-screen=true
-# This uses Phosophorus and Midori to run O by DCPU-16
