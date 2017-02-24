45d1f22dee1ded7096a2ea23ae4c7171e87dc783
Scottt <scott.geyer@eagles.usm.edu>
2017-02-23 18:58:02 -0600

    Update Epic-Ninja.sh

--- Epic-Ninja.sh
+++ Epic-Ninja.sh
@@ -5,3 +5,17 @@ xset s noblank # don't blank the video device
 matchbox-window-manager &
 midori -e Fullscreen -a https://phosphorus.github.io/app.html?id=21554369&turbo=false&full-screen=true
 # This uses Phosophorus and Midori to run Epic Ninja by GriffPatch
+
+#Keyboard
+#Arrow WASD - Move
+# UP/W - Jump
+# Space - Attack
+# T - Throw
+# P - Pause
+# M - Music On/Off
+
+#Joystick:
+# Button 1,2 (A,B) = Jump
+# Buton 4 (Y) = Attack
+# Button 3 (X) = Throw
+# Button 7 (Select) = Music on/off
