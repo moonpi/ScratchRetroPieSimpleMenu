84daa9b16e0ee36f9f15680c3801b3bd29a1dabb
Scottt <scott.geyer@eagles.usm.edu>
2017-02-23 19:06:39 -0600

    Update FOIL-Mario.sh

--- FOIL-Mario.sh
+++ FOIL-Mario.sh
@@ -3,6 +3,6 @@
 xset -dpms # disable DPMS (Energy Star) features.
 xset s off # disable screen saver
 xset s noblank # don't blank the video device
-matchbox-window-manager &
+matchbox-window-manager & qjoypad "FOIL-Mario" &
 midori -e Fullscreen -a https://phosphorus.github.io/app.html?id=146878543&turbo=false&full-screen=true
 # This uses Phosophorus and Midori to run FOIL Mario by Punkbass (remixed from FlunkyLizardBun and PaulKoning)
